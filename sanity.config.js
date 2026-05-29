f60839ca738d1b5e36c5276d64a4cd6953b89710
Benjamin Corette <270361094+BenCor02@users.noreply.github.com>
2026-05-29 23:19:27 +0200

    fix: sanity/plugins/structure → sanity/structure (v5 API)

--- sanity.config.js
+++ sanity.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/plugins/structure'
+import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import article from './sanity/schemas/article.js'
 
